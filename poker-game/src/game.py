cd0baf4128585fa26fcad415857d75613353d2f5
José Manuel Peña Rodríguez <145053972+jpprguezz@users.noreply.github.com>
2025-04-17 21:04:59 +0100

    Add files via upload

--- poker-game/src/game.py
+++ poker-game/src/game.py
@@ -1,2 +1,14 @@
+from cards import Card
+
 def get_winner(players: list[Player], common_cards: list[Card], private_cards: list[list[Card]])-> tuple[Player | None, Hand]:
+    player1 = players[0]
+    player2 = players[1]
+    player1_cards = private_cards[0]
+    player2_cards = private_cards[1]
+    pass
+
+def get_common_cards(common_cards: list[Card]):
+    return common_cards
+
+
     
--- poker-game/src/roles.py
+++ poker-game/src/roles.py
@@ -1,4 +1,22 @@
+from __future__ import annotations
+from helpers import combinations
+import card
+import game
+
 class Player:
     def __init__(self, name: str):
-         self.name = name
+        self.name = name
+        self.private_cards = []
+
+    def obtain_best_hand(self):
+        available_cards = self.private_cards + game.get_common_cards
+        all_posible_combinations = list(combinations(available_cards, n=5))
+        first_combination = all_posible_combinations[0]
+        for combination in all_posible_combinations:
+            if combination > first_combination:
+                first_combination = combination
+            elif combination == first_combination
+        
+
+
 
