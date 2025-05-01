35b559df9bf42d25c9016cf9600c6e45984f4a49
José Manuel Peña Rodríguez <145053972+jpprguezz@users.noreply.github.com>
2025-05-01 18:19:09 +0000

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,14 +1,29 @@
-from cards import Card
+from __future__ import annotations
+from src.cards import Card, Hand
+from src.roles import Player
+import src.helpers
 
-def get_winner(players: list[Player], common_cards: list[Card], private_cards: list[list[Card]])-> tuple[Player | None, Hand]:
-    player1 = players[0]
-    player2 = players[1]
-    player1_cards = private_cards[0]
-    player2_cards = private_cards[1]
-    pass
+def get_best_hand(cards: list[Card]) -> Hand:
+    best = None
+    for combination in src.helpers.combinations(cards, n=5):
+        hand = Hand(list(combination), '0', '')
+        cat, cat_rank = hand.winner_category()
+        hand.cat = str(cat)
+        hand.cat_rank = cat_rank
+        if best is None or hand > best:
+            best = hand
+    return best
 
-def get_common_cards(common_cards: list[Card]):
-    return common_cards
 
-
+def get_winner(
+    players: list[Player], common_cards: list[Card], private_cards: list[list[Card]]
+) -> tuple[Player | None, Hand]:
+        
+        hand1 = get_best_hand(private_cards[0] + common_cards)
+        hand2 = get_best_hand(private_cards[1] + common_cards)
+        if hand1 > hand2:
+            return players[0], hand1
+        elif hand2 > hand1:
+            return players[1], hand2
+        return None, hand1
     
